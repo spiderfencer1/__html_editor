42a623e7e45a33b0fe3a0a52f2978aae99faaf86
spiderfencer1 <jinjoo@gmail.com>
2017-09-12 10:21:56 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+window.onload = () =>
+{
+  function id(i){return document.getElementById(i);}
+  function tag(t){return document.getElementsByTagName(t);}
+  
+  id('run').onclick = () =>
+  {
+    const doc = id('output').contentWindow.document;
+    doc.open();
+    doc.write(id('input').value);
+    doc.close();
+  }
+  id('save').onclick = () =>
+  {
+    const el = document.createElement('a');
+    el.setAttribute('href','data:text/plain;charset=utf-8,' + encodeURIComponent(id('input').value));
+    el.setAttribute('download',id('fname').value);
+    el.style.display = 'none';
+    document.body.appendChild(el);
+    el.click();
+    document.body.removeChild(el);
+  }
+};
